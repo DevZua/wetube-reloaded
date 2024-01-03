963c0b7646791e96f16250bb5a544e1020db761c
DevZua <gimzua0228@gmail.com>
2024-01-04 02:45:16 +0900

    first upload

---
+++ src/controllers/videoController.js
@@ -0,0 +1,11 @@
+export const trending = (req, res) => res.render("home");
+
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
+export const search = (req, res) => res.send("Search");
+export const upload = (req, res) => res.send("Upload");
+export const deleteVideo = (req, res) => {
+    console.log(req.params);
+    return res.send("Delete Video");
+}
+
---
+++ src/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
+
+const PORT = 4000;
+
+console.log(process.cwd());
+
+const app = express();
+const logger = morgan("dev");
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+const handleListening = () => 
+    console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
+
